c81c909a0712f8bd22fe0541513981ebf5832897
casumelanie <melanie.casu-pro@outlook.fr>
2023-11-01 21:19:01 +0100

    Récupération et ajout des travaux depuis le back

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,7 +1,18 @@
-console.log('test')
-
-const methodTest = () => {
-    console.log('test')
+async function recupererEtCreerProjets () {
+    const reponse = await fetch('http://localhost:5678/api/works')
+    const projets = await reponse.json()
+    projets.forEach(element => {
+        /* console.log(element) */
+        const projectGallery = document.querySelector('#portfolio .gallery')
+        const projectCard = document.createElement('figure')
+        const projectImg = document.createElement('img')
+        projectImg.src = element.imageUrl
+        projectImg.alt = element.title
+        const projectTitle = document.createElement('figcaption')
+        projectTitle.innerText = element.title
+        projectCard.appendChild(projectImg)
+        projectCard.appendChild(projectTitle)
+        projectGallery.appendChild(projectCard)
+    })
 }
-
-methodTest()
+recupererEtCreerProjets()
