35f973456d6894510326cfbe6e85985fc80ff262
casumelanie <melanie.casu-pro@outlook.fr>
2023-11-16 21:58:15 +0100

    Mise à jour de la page d'accueil lorsque l'utilisateur est connecté

--- FrontEnd/scripts/domLinker.js
+++ FrontEnd/scripts/domLinker.js
@@ -1,8 +1,12 @@
 // LES APPELS EFFECTUES SUR LE DOM
+
+export const body = document.querySelector('body')
+export const loginButton = document.getElementById('login-link')
+
 export const projectGallery = document.querySelector('#portfolio .gallery')
 export const filterContainer = document.querySelector('#portfolio .portfolio-filters')
 
-// fais buguer la remonté de projet si pas commenté et pas utilisé
+// ELEMENTS DU FORMULAIRE DE CONNEXION
 export const loginEmail = document.getElementById('mail')
 export const loginPassword = document.getElementById('password')
 export const formLogin = document.getElementById('login-form')
--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,32 +1,28 @@
 import { postLogin } from './api.js'
 import { loginEmail, loginPassword, formLogin } from './domLinker.js'
-localStorage.clear()
 
-// Fonction de connexion
-const connectionFunction = async () => {
+// Lancement de la fonction connexion au clic sur submit
+formLogin.addEventListener('submit', async function (e) {
+    e.preventDefault()
+    // Récupération de l'user et du mdp renseignés
     const user = {
         email: loginEmail.value,
         password: loginPassword.value
     }
 
-    // Methode post connexion
-    postLogin(user)
-        .then(data => {
-            if (data.token) {
-                // Stockage du token dans le local storage
-                const token = data.token
-                localStorage.token = token
-                console.log(localStorage)
-                // Redirection vers la page d'accueil
-                document.location.href = 'index.html'
-            } else {
-                alert('Erreur dans l\'identifiant ou le mot de passe')
-            }
-        }).catch(error => alert('API non disponible : ' + error.message))
-}
-
-// Lancement de la fonction connexion au clic sur le bouton de connexion
-formLogin.addEventListener('submit', function (e) {
-    e.preventDefault()
-    connectionFunction()
+    try {
+        // Appel vers l'API
+        const data = await postLogin(user)
+        // En cas de réussite, stockage du token dans le local Storage + redirection
+        if (data.token) {
+            const token = data.token
+            localStorage.token = token
+            console.log(localStorage)
+            document.location.href = 'index.html'
+        } else {
+            alert('Erreur dans l\'identifiant ou le mot de passe')
+        }
+    } catch (error) {
+        alert('API non disponible : ' + error.message)
+    }
 })
